be5e6fb7fee46041d44325fd5c23064aa18649de
Antti Rautanen <anttirautanen@users.noreply.github.com>
2026-02-24 15:54:53 +0200

    Add a store for handling popups

--- front/src/Map.tsx
+++ front/src/Map.tsx
@@ -3,8 +3,24 @@ import { useEffect } from "react"
 import TileLayer from "ol/layer/Tile"
 import { OSM } from "ol/source"
 import { Map, View } from "ol"
+import { create } from "zustand"
+import type { Coordinate } from "ol/coordinate"
+
+interface Store {
+  popupIsVisible: boolean
+  popupPosition: Coordinate
+  showPopup: (position: Coordinate) => void
+}
+
+const useStore = create<Store>((set) => ({
+  popupIsVisible: false,
+  popupPosition: [0, 0],
+  showPopup: (position: Coordinate) => set((state) => ({ ...state, popupPosition: position, popupIsVisible: true })),
+}))
 
 export const MapView = () => {
+  const { popupIsVisible, popupPosition, showPopup } = useStore()
+
   useEffect(() => {
     const map = new Map({
       target: "map",
@@ -19,9 +35,14 @@ export const MapView = () => {
       }),
     })
 
+    map.on("singleclick", (event) => {
+      showPopup(event.coordinate)
+    })
+
     return () => {
       map.setTarget()
     }
   }, [])
+
   return <div className="fixed top-0 right-0 left-0 bottom-0" id="map" />
 }
