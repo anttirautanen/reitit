2f946f63ec1583e0635e74d50e4699e78040d3b8
Antti Rautanen <anttirautanen@users.noreply.github.com>
2026-02-26 11:28:52 +0200

    Persist origin and destination to local storage

--- front/src/useStore.ts
+++ front/src/useStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand"
 import type { Coordinate } from "ol/coordinate"
+import { persist } from "zustand/middleware"
 
 interface Store {
   origin: Coordinate | null
@@ -8,9 +9,17 @@ interface Store {
   setDestination: (destination: Coordinate) => void
 }
 
-export const useStore = create<Store>((set) => ({
-  origin: null,
-  destination: null,
-  setOrigin: (origin: Coordinate) => set({ origin }),
-  setDestination: (destination: Coordinate) => set({ destination }),
-}))
+export const useStore = create<Store>()(
+  persist(
+    (set) => ({
+      origin: null,
+      destination: null,
+      setOrigin: (origin: Coordinate) => set({ origin }),
+      setDestination: (destination: Coordinate) => set({ destination }),
+    }),
+    {
+      name: "reitit-store",
+      partialize: (state) => ({ origin: state.origin, destination: state.destination }),
+    }
+  )
+)
